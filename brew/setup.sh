bba58425e5a916d99353be733f9b923db338a98a
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2017-10-11 00:03:27 +0700

    :sparkles: Add homebrew setup script

---
+++ brew/setup.sh
@@ -0,0 +1,42 @@
+# Environment
+echo "##### Installing Environments..."
+brew cask install java
+brew install node
+brew install yarn
+
+# Command Line Interface
+echo "##### Installing CLIs..."
+brew install heroku
+brew install unrar
+brew install z
+
+# Editor
+echo "##### Installing Editors..."
+brew cask install atom
+brew cask install textmate
+
+# Development Tools
+echo "##### Installing Development Tools..."
+brew cask install eclipse-java
+brew cask install netbeans-java-se
+brew cask install android-studio
+brew cask install sequel-pro
+brew cask install sourcetree
+brew cask install postman
+brew cask install docker
+
+# Browser
+echo "##### Installing Browsers..."
+brew cask install google-chrome
+
+# Messenger
+echo "##### Installing Messengers..."
+brew cask install skype
+
+# Entertainment
+echo "##### Installing Entertainments..."
+brew cask install vlc
+
+# Utilities
+echo "##### Installing Utilities..."
+brew cask install teamviewer
