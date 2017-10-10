031b19be4ceed03f0de17e1cc79158cfed1ce2fe
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2017-10-11 00:06:25 +0700

    :sparkles: Add installing scripts

--- setup-mac.sh
+++ setup-mac.sh
@@ -1,50 +1,99 @@
-#!/bin/sh
-# To execute: save and `chmod +x ./brew-install-script.sh` then `./brew-install-script.sh`
+# Created by Kongpon Charanwattanakit
+# https://github.com/kykungz
 
-echo "Installing brew..."
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+echo_red() {
+  printf "##### ${RED}${1}${NC}\n"
+}
+
+echo_green() {
+  printf "##### ${GREEN}${1}${NC}\n"
+}
+
+echo_yellow() {
+  printf "##### ${YELLOW}${1}${NC}\n"
+}
+
+# Check for existance of Xcode
+if [ ! -d "/Applications/Xcode.app" ]; then
+  echo "Xcode is required, try installing from App Store before running the script."
+  exit 1
+fi
+
+echo_green "Creating ~/Developer directory in $HOME"
+mkdir ~/Developer
+
+echo;echo;
+
+echo_green "Opening Xcode Command Line Tools installation..."
+xcode-select --install
+
+echo;echo;
+
+echo_green "Installing brew..."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap caskroom/cask
-echo 'Installing oh-my-zsh'
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+brew doctor
+brew update
+
+echo;echo;
+
+echo_green "Installing oh-my-zsh..."
 brew install zsh zsh-completions
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp -Rf ./zsh/themes ~/.oh-my-zsh/custom
+cp -f ./zsh/.zshrc ~/.zshrc
+cat ./zsh/_alias >> ~/.zshrc
+cat ./zsh/_functions >> ~/.zshrc
+cat ./zsh/_dependencies >> ~/.zshrc
 
-echo 'Installing iterm2....'
+echo;echo;
+
+echo_green "Installing iterm2..."
 brew cask install iterm2
+cp -f ./iterm/com.googlecode.iterm2.plist ~/Library/Preferences/
 
-# Editor
-brew install neovim/neovim/neovim
-brew cask install caskroom/cask/visual-studio-code
+echo;echo;
 
-# Dev tools
-brew cask install robomongo
-brew cask search postman
-brew cask install graphiql
+echo_green "Running brew/setup.sh..."
+./brew/setup.sh
 
-# Browser
-brew cask install google-chrome
-brew cask install firefox
+echo;echo;
 
-# Messenger
-brew cask install telegram
-brew cask install slack
+echo_green "Running atom/setup.sh..."
+./atom/setup.sh
 
-# Utilities
-brew install unrar
-brew install p7zip
-brew cask install sourcetree
-brew cask install dropbox
-brew cask install transmit
-brew cask install evernote
-brew cask install parallels-desktop
-brew cask install microsoft-office
-brew cask install duet
-brew install wine --without-x11
-brew install mono
+echo;echo;
 
-# Design
-brew cask install sketch
+echo_green 'Your setup is about to finish!'
+echo_green 'Just a few configurations and you are ready to go!'
 
-# Entertain
-brew cask install openemu
-brew cask install xld 
-brew install ecm
+echo;echo;
+
+echo_green "Setting up your git profile..."
+echo_green "Enter your GitHub name:"
+read -r name
+git config --global user.name "${name}"
+echo_green "Enter your GitHub email:"
+read email
+git config --global user.email ${email}
+
+echo;echo;
+
+echo_green "Enter your password then hit Enter"
+echo_green "in order to change the default shell."
+chsh -s /bin/zsh
+
+echo;echo;
+
+echo_green 'Installation finished !!'
+echo_green 'Try to restart your Mac complete the process.'
+echo_green 'Enjoy your new Mac !!'
+
+open -a "/Applications/Google Chrome.app" 'https://github.com/kykungz/environment-setup/blob/master/UNLISTED.md'
+
+rm -rf ~/environment-setup
