a8dea5258ccbc7ce8f05f5c6d6cfbae769eacf55
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2017-10-11 00:03:06 +0700

    :sparkles: Add atom setup script

---
+++ atom/setup.sh
@@ -0,0 +1,30 @@
+echo "##### Installing Atom packages..."
+
+apm install advanced-open-file
+apm install atom-beautify
+apm install atom-bootstrap4
+apm install atom-html-preview
+apm install atom-jest-snippets
+apm install atom-ternjs
+apm install autocomplete-modules
+apm install autocomplete-python
+apm install busy-signal
+apm install color-picker
+apm install emmet
+apm install file-icons
+apm install highlight-selected
+apm install intentions
+apm install jquery-snippets
+apm install language-vue
+apm install language-x86-64-assembly
+apm install linter
+apm install linter-clang
+apm install linter-eslint
+apm install linter-ui-default
+apm install minimap
+apm install pigments
+apm install platformio-ide-terminal
+apm install react
+apm install script
+apm install todo-show
+apm install vue-format
