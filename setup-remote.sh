95b7a9c1205aebab8cb62d4d945e45b700235f23
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2017-10-29 20:54:42 +0700

    :sparkles: Remove directory before cloning

--- setup-remote.sh
+++ setup-remote.sh
@@ -7,6 +7,7 @@ NC='\033[0m' # No Color
 if type xcode-select >&- && xpath=$( xcode-select --print-path ) &&
    test -d "${xpath}" && test -x "${xpath}" ; then
    #... is correctly installed
+   rm -rf ~/environment-setup
    git clone https://github.com/kykungz/environment-setup.git ~/environment-setup
    echo;echo;
    bash ~/environment-setup/setup-mac.sh
@@ -14,6 +15,7 @@ if type xcode-select >&- && xpath=$( xcode-select --print-path ) &&
 else
    #... isn't correctly installed
    printf "##### ${RED}Xcode Command Line Tools is required.${NC}\n"
+   printf "##### ${RED}Run the script again after finished installing.${NC}\n"
    printf "##### ${GREEN}Opening installation...${NC}"
    xcode-select --install
 fi
