c80dccb13df7e0da926eec6a74f4e014faf6416c
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2017-10-30 01:40:08 +0700

    :sparkles: Add waiting for Command Line Tools installation

--- setup-remote.sh
+++ setup-remote.sh
@@ -5,27 +5,29 @@ NC='\033[0m' # Reset (No Color)
 MODULE=$0
 
 # Check for existance of Xcode Command Line Interface
-if type xcode-select >&- && xpath=$( xcode-select --print-path ) && test -d "${xpath}" && test -x "${xpath}" ; then
-  #... is correctly installed
-  rm -rf ~/environment-setup
-  git clone https://github.com/kykungz/environment-setup.git ~/environment-setup
-  echo;echo;
-  if [ "$MODULE" ]; then
-    bash -c "
-      source ~/environment-setup/utils.sh;
-      prompt_install;
-      source ~/environment-setup/${MODULE}/setup.sh
-      echoecho;
-      echo_green Done!"
-  else
-    bash ~/environment-setup/setup-mac.sh
-  fi
-  rm -rf ~/environment-setup
-else
-  #... isn't correctly installed
-  echo;echo;
+if [ ! -d "$(xcode-select -p)" ]; then
   printf "##### ${RED}Xcode Command Line Tools is required.${NC}\n"
-  printf "##### ${RED}Run the script again after finished installing.${NC}\n"
   printf "##### ${GREEN}Opening installation...${NC}"
-  xcode-select --install
+  xcode-select --install &> /dev/null
+  while ! xcode-select -p &> /dev/null; do
+    sleep 5
+  done
+  printf "##### ${GREEN}Done installing Xcode Command Line Tools.${NC}"
 fi
+
+rm -rf ~/environment-setup
+git clone https://github.com/kykungz/environment-setup.git ~/environment-setup
+echo;echo;
+
+if [ "$MODULE" ]; then
+  bash -c "
+    source ~/environment-setup/utils.sh;
+    prompt_install;
+    source ~/environment-setup/${MODULE}/setup.sh
+    echoecho;
+    echo_green Done!"
+else
+  bash ~/environment-setup/setup-mac.sh
+fi
+
+rm -rf ~/environment-setup
