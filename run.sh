53f00b47a5bef7ca4338777b8c97b24ac4b13765
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2025-06-25 21:35:35 +0700

    feat(run.sh): Add zsh plugins for autosuggestions

--- run.sh
+++ run.sh
@@ -22,3 +22,7 @@ cp ./com.googlecode.iterm2.plist $HOME/Library/Preferences/
 
 # ZSH
 cp ./.zshrc $HOME/
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/lukechilds/zsh-nvm ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-nvm
