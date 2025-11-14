3823c88f5619c2ade334e78cca7dbbd596acdd70
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2025-11-14 20:20:30 +0700

    feat(plugins): add new plugins.yaml file for k9s with pretty logs commands fix(update.sh): update script to copy plugins.yaml from user's k9s directory fix(settings.json): add gitlens views configuration for better SCM visibility fix(iterm2): update com.googlecode.iterm2.plist to reflect recent changes

--- update.sh
+++ update.sh
@@ -4,5 +4,6 @@ cp "$HOME/.p10k.zsh" .
 cp "$HOME/.zshrc" .
 cp "$HOME/Library/Preferences/com.googlecode.iterm2.plist" .
 cp "$HOME/Library/Application Support/Cursor/User/keybindings.json" .
+cp "$HOME/Library/Application Support/k9s/plugins.yaml" .
 jq -S . "$HOME/Library/Application Support/Cursor/User/settings.json" > settings.json
 cursor --list-extensions > extensions.txt
